2c2b5b1ddeaee400b5cc2dfa42c3a50c80e341ac
Бейбарыс Жеткергенов <bejbaryszetkergenov@MacBook-Air-Gulfajruz.local>
2026-02-25 00:16:37 +0500

    fix railway port

--- main.py
+++ main.py
@@ -397,3 +397,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")
 
 app.mount("/", StaticFiles(directory=FRONTEND_DIR, html=True), name="frontend")
+
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
