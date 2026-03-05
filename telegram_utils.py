6b61fb564e4e29466a8fe3d3e3671dd8bb196abc
Бейбарыс Жеткергенов <bejbaryszetkergenov@MacBook-Air-Gulfajruz.local>
2026-03-06 00:05:41 +0500

    fix: telegram bot token environment variable issue

--- telegram_utils.py
+++ telegram_utils.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 def send_telegram(message):
 
-    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+    bot_token = os.getenv("TELEGRAM_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    print("BOT:", os.getenv("TELEGRAM_BOT_TOKEN"))
+    print("BOT:", os.getenv("TELEGRAM_TOKEN"))
     print("CHAT:", os.getenv("TELEGRAM_CHAT_ID"))
     if not bot_token or not chat_id:
         print("TELEGRAM ERROR: TOKEN OR CHAT_ID MISSING")
